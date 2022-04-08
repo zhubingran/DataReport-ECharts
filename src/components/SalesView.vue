919f5d48a5a125c318b91ce826b5cd5dfd5ad068
zhubingran <zbingran@126.com>
2022-04-08 14:16:01 +0800

    引入echarts插件

--- src/components/SalesView.vue
+++ src/components/SalesView.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>Sales View</div>
+  <div class="sales-view">
+    <el-card shadow="hover">
+      <template v-slot:header>
+        <div class="menu-wrapper">
+          <el-menu mode="horizontal">
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+        </div>
+      </template>
+      <template>
+        <div>222</div>
+      </template>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.sales-view {
+  margin-top: 20px;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import { Card,Row,Col } from 'element-ui'
+import { Card,Row,Col,Menu,MenuItem,RadioGroup,Radio,DatePicker } from 'element-ui'
 
 
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(Radio)
+Vue.use(DatePicker)
