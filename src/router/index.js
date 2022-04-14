ad424c76305d98f336e48c72ff1f45e5e386d3fc
zhubingran <zbingran@126.com>
2022-04-14 23:01:18 +0800

    引入百度地图

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BMap from '../views/BMap.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path:'/bmap',
+    name:'BMap',
+    component:BMap
   }
 ]
 
